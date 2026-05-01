7cad85324a785a5451e4d1d4430108a9c79a8d0b
Tanvir <sabbirtanvirahmed18@gmail.com>
2026-05-01 13:28:16 +0600

    Practice Non primitive data type

---
+++ non_PrimitiveType.ts
@@ -0,0 +1,13 @@
+// Non-Primitive_Type
+
+let flower:string[] = ['golap', 'joba', 'rojonigondha']
+flower.push('podda')
+// flower.pop()
+console.log(flower)
+
+// ts tuple type
+let Electronics:(string|number)[] = ['iPhone 17',170000, 'Computer',100000]
+console.log(Electronics)
+
+let Couple:[string,string]= ['jamai','bow']
+console.log(Couple)
--- primitive_dataType.ts
+++ primitive_dataType.ts
@@ -1,10 +1,18 @@
 // Data Type Primitive
 // string
-let UserName: string= 'Sabbir'
-UserName='mohon'
-console.log(UserName)
+let UserName: string= 'Sabbir';
+UserName='mohon';
+console.log(UserName);
 
 // Number Type
-let productId:number = 127
-productId = 1111
-console.log(productId)
+let productId:number = 127;
+productId = 1111;
+console.log(productId);
+
+// bOOlean
+let ActiveUser:boolean = false;
+ActiveUser=true;
+
+// Eorror Show ActiveUser= 1212 
+console.log(ActiveUser)
+
