dad0d8a6f6116168e805ff960a71b9d380082a9d
Tanvir <sabbirtanvirahmed18@gmail.com>
2026-05-01 14:31:59 +0600

    Practice  Reference obj type optional type and literal type

--- non_PrimitiveType.ts
+++ non_PrimitiveType.ts
@@ -1,13 +1,32 @@
 // Non-Primitive_Type
 
-let flower:string[] = ['golap', 'joba', 'rojonigondha']
+let flower: string[] = ['golap', 'joba', 'rojonigondha']
 flower.push('podda')
 // flower.pop()
 console.log(flower)
 
 // ts tuple type
-let Electronics:(string|number)[] = ['iPhone 17',170000, 'Computer',100000]
+let Electronics: (string | number)[] = ['iPhone 17', 170000, 'Computer', 100000]
 console.log(Electronics)
 
-let Couple:[string,string]= ['jamai','bow']
+let Couple: [string, string] = ['jamai', 'bow']
 console.log(Couple)
+
+
+// Reference obj type optional type and literal type 
+
+let User: {
+    company:'ZentroMart'
+    fristName: string;
+    middelname?: string;    
+    lastName: string;
+    isMarried:boolean
+
+} = {
+    fristName: 'Tanvir',
+    middelname: 'Ahmed',
+    lastName: 'Sabbir',
+    isMarried: true,
+    company:'ZentroMart' 
+}
+console.log(User)
